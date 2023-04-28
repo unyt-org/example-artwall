9b6a13799032c87c0b064880bf8cfe77250a5339
Jonas Strehle <jonas@strehles.info>
2023-04-28 17:14:19 +0200

    add: logger

--- backend/entrypoint.tsx
+++ backend/entrypoint.tsx
@@ -3,6 +3,9 @@ import { $$, anonymous, Datex } from "unyt_core";
 import { GameMap } from "common/GameMap.ts";
 
 const scheduler = new Datex.UpdateScheduler(50);
+Datex.Compiler.SIGN_DEFAULT = false;
+
+Datex.MessageLogger.enable();
 
 export const matrix = await lazyEternal ?? $$(
 	new GameMap(SIZE.width, SIZE.height)
@@ -18,6 +21,6 @@ export async function getAreaIndex() {
 		return -1;
 	if (areaMap.has(endpoint))
 		return areaMap.get(endpoint);
-	const index = Math.max(...areaMap.values(), 1) + 1;
+	const index = Math.max(...areaMap.values(), 0) + 1;
 	return await areaMap.set(endpoint, index), index;
 }
